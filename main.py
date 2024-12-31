acfe9840a2b6ab3cc3f1fac60ea76cc63c9c96a5
Manish <m4manishp4prasad@gmail.com>
2024-12-31 18:09:00 +0530

    Update

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 
 if __name__ == '__main__':
     print("Hii")
+
+
+from config.db_config import ConnectionToDB
+
+if __name__ == '__main__':
+    db_obj=ConnectionToDB()
+    db_obj.create_connection()
